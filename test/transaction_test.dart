30953f915cee0c3881d6b11efa02fb37ab91d05d
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-26 09:41:31 -0200

    1.3

---
+++ test/transaction_test.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/models/transaction.dart';
+
+void main(){
+  test('Should return the value when create a transaction', (){
+    final transaction = Transaction(null, 200, null);
+    expect(transaction.value, 200);
+  });
+  test('Should show error when create transaction with value less than zero', (){
+    expect(() => Transaction(null, 0, null), throwsAssertionError);
+  });
+}
