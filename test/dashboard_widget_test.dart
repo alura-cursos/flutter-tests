fa77feead665ec4bf2d34fe1873ba24fec3af643
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-26 11:12:56 -0200

    2.2

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -8,9 +8,11 @@ void main(){
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
-  testWidgets('Should display the first feature when the Dashboard is opened', (tester) async {
+  testWidgets('Should display the transfer feature when the Dashboard is opened', (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final firstFeature = find.byType(FeatureItem);
-    expect(firstFeature, findsWidgets);
+    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+    expect(iconTransferFeatureItem, findsOneWidget);
+    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
+    expect(nameTransferFeatureItem, findsOneWidget);
   });
 }
